92d255d9d1b90a7867ef6eacf3443179ebf21ffe
MorganBrownDev <mjxbrown@gmail.com>
2023-05-20 15:02:50 +1000

    Page links updated

--- src/_includes/components/Card.js
+++ src/_includes/components/Card.js
@@ -5,7 +5,7 @@ function Card ({title, image, link, linkText, raised}) {
     return html`
     <div class="${`card-container ${raised ? `card-container-active` : ``}`}" style="background-image: url(${image});">
         <h1 class="card-title">${title}</h1>
-        <a href="${link}" target="_blank" class="card-link">${linkText}</a>
+        <a href="${link}" class="card-link">${linkText}</a>
     </div>
     `
 }
