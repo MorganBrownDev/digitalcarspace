1c33b608c65585647a892dfe236d734c6891c4dd
Morgan Brown <mjxbrown@gmail.com>
2023-08-18 01:09:17 +1000

    Update

--- src/_includes/components/Card.js
+++ src/_includes/components/Card.js
@@ -3,7 +3,7 @@ const {html} = require('common-tags');
 function Card ({title, image, link, linkText, raised}) {
 
     return html`
-    <a href="${link}"
+    <a href="${link}>"
         <div class="${`card_container ${raised ? `card_container-active` : ``}`}" style="background-image: url(${image});">
             <h1 class="card_title">${title}</h1>
         </div>
