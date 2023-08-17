02e0ee95000b0f337848e68806ffe0bc3d68454b
Morgan Brown <mjxbrown@gmail.com>
2023-08-18 00:49:58 +1000

    Minor style updates
    Minor style updates 17/08/2023 - 18/08/2023

--- src/_includes/components/Card.js
+++ src/_includes/components/Card.js
@@ -3,9 +3,9 @@ const {html} = require('common-tags');
 function Card ({title, image, link, linkText, raised}) {
 
     return html`
-    <div class="${`card-container ${raised ? `card-container-active` : ``}`}" style="background-image: url(${image});">
-        <h1 class="card-title">${title}</h1>
-        <a href="${link}" class="card-link">${linkText}</a>
+    <div class="${`card_container ${raised ? `card_container-active` : ``}`}" style="background-image: url(${image});">
+        <h1 class="card_title">${title}</h1>
+        <a href="${link}" class="card_link">${linkText}</a>
     </div>
     `
 }
