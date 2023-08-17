5a78899633fa56d8a514ac35d64b27d1b11eceab
Morgan Brown <mjxbrown@gmail.com>
2023-08-18 01:04:44 +1000

    Grid style adapted to match large breakpoint

--- src/_includes/components/Card.js
+++ src/_includes/components/Card.js
@@ -3,10 +3,11 @@ const {html} = require('common-tags');
 function Card ({title, image, link, linkText, raised}) {
 
     return html`
-    <div class="${`card_container ${raised ? `card_container-active` : ``}`}" style="background-image: url(${image});">
-        <h1 class="card_title">${title}</h1>
-        <a href="${link}" class="card_link">${linkText}</a>
-    </div>
+    <a href="${link}"
+        <div class="${`card_container ${raised ? `card_container-active` : ``}`}" style="background-image: url(${image});">
+            <h1 class="card_title">${title}</h1>
+        </div>
+    </a>
     `
 }
 
